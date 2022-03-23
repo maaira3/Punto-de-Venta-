7178bf9c4624d4aeb52c05f13c3990924e287908
Maira Jiménez <85815960+maaira3@users.noreply.github.com>
2022-03-23 04:55:10 -0600

    Add files via upload

---
+++ cliente/src/pages/Login/Components/Form.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Login from '../Login'
+import '../../../styles.scss'
+import Clock from './Time'
+import ScheduleIcon from '@mui/icons-material/Schedule'
+
+function Form(){
+    return (
+    <>
+    <div className="ocean">
+	    <div className="wave"></div>
+	    <div className="wave"></div>
+    </div>
+      <div className='form-container'>
+        <div className='form-content-left'>
+        <ScheduleIcon className="login-cal-icon"/>
+        <Clock/>
+        </div>
+        <Login/>
+      </div>
+    </>
+  );
+};
+
+export default Form
